cca1c3ac76df7c8c5e961480caec6fdfe6111f23
sakib <sbsakibsarkar8@gmail.com>
2025-02-01 20:03:56 +0600

    add ing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
 import './App.css'
-
+import { CiMicrophoneOn } from "react-icons/ci";
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-    Hello world!
-  </h1>
+    <div className=' w-full h-full overflow-hidden flex justify-center items-center flex-col gap-9'>
+      <img className=' h-80 py-10' src="/ai.png" alt="" />
+      <span className=' text-white'>I am Shafra, your virtual assistant</span>
+      <button className=' w-[180px] h-[40px] text-black flex bg-green-500 justify-center items-center gap-4 border-none rounded-full cursor-pointer font-bold text-[20px]'>Click here <CiMicrophoneOn className=' h-[30px] w-[30px] ' /></button>
+    </div>
   )
 }
 
