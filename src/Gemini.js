d360646973d0e2929df1108c8d36afc5018fe266
sakib <sbsakibsarkar8@gmail.com>
2025-02-02 19:20:54 +0600

    add api

---
+++ src/Gemini.js
@@ -0,0 +1,17 @@
+let Apikey = "AIzaSyC4fi_yB9dfolH3H4QtVDqbv1pPxlmL4xk"
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+// Set API Key
+const genAI = new GoogleGenerativeAI(Apikey);
+
+async function getResponse(prompt) {
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  console.log(response.text());
+}
+
+// Example usage
+export default getResponse("Hello, how are you?");
