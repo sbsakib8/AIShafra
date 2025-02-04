f73b5262ac2532454851cf8f85a0db65f0f80be7
sakib <sbsakibsarkar8@gmail.com>
2025-02-04 21:29:03 +0600

    add website link

--- src/comtext/UseContext.jsx
+++ src/comtext/UseContext.jsx
@@ -33,7 +33,58 @@ function UseContext({children}) {
         let transcript = e.results[currentIndex][0].transcript
 
        setListening(transcript)
-        aiResponse(transcript)
+       takecommand(transcript.toLowerCase())
+    }
+
+
+    // open the recognition
+    function takecommand(command){
+        if(command.includes('open')&&command.includes('youtube')){
+            window.open('https://www.youtube.com/','_blank')
+            speak('opening youtube...')
+            setListening('opening youtube...')
+            setTimeout(()=>{
+                setspeaking(false)
+           },5000)
+        }else if(command.includes('open')&&command.includes('google')){
+            window.open('https://www.google.com/','_blank')
+            speak('opening google...')
+            setListening('opening google...')
+            setTimeout(()=>{
+                setspeaking(false)
+           },5000)
+        }else if(command.includes('open')&&command.includes('facebook')){
+            window.open('https://www.facebook.com/','_blank')
+            speak('opening facebook...')
+            setListening('opening facebook...')
+            setTimeout(()=>{
+                setspeaking(false)
+           },5000)
+        }else if(command.includes('open')&&command.includes('linkedin')){
+            window.open('https://www.linkedin.com/','_blank')
+            speak('opening linkedin...')
+            setListening('opening linkedin...')
+            setTimeout(()=>{
+                setspeaking(false)
+           },5000)
+        }else if(command.includes('open')&&command.includes('instagram')){
+            window.open('https://www.instagram.com/','_blank')
+            speak('opening instagram...')
+            setListening('opening instagram...')
+            setTimeout(()=>{
+                setspeaking(false)
+           },5000)
+        }else if(command.includes('open')&&command.includes('messenger')){
+            window.open('https://www.messenger.com/','_blank')
+            speak('opening messenger...')
+            setListening('opening messenger...')
+            setTimeout(()=>{
+                setspeaking(false)
+           },5000)
+        }
+        else{
+            aiResponse(command)
+        }
     }
     const value = {
         recognition,
